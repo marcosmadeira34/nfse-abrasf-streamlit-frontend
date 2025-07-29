6e934b8a4137f751b94ac3fd1bf2d3da140e0431
Marcos Madeira <mmadeirasilva5@gmail.com>
2025-07-29 18:42:45 -0300

    alter homepage.html

--- aqua-file-transform/src/pages/Homepage.tsx
+++ aqua-file-transform/src/pages/Homepage.tsx
@@ -1,9 +1,15 @@
 // src/pages/Home.tsx
+import { useEffect } from 'react';
+
 export default function Home() {
+  useEffect(() => {
+    console.log('Home component loaded');
+  }, []);
+
   return (
     <iframe
       src="homepage.html"
-      style={{ width: "100%", height: "100vh", border: "none" }}
+      style={{ width: '100%', height: '100vh', border: 'none' }}
       title="Página Home"
     />
   );
